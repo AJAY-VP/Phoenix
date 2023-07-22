432791f02c5f3ec07f1f6097660ba5cf06004bb9
Ajay V P <vpajay.99@gmail.com>
2023-07-22 11:42:55 +0530

    version update angular

--- frontend/src/app/pages/auth/login/login.component.ts
+++ frontend/src/app/pages/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators, ValidatorFn } from '@angular/forms';
+import { UntypedFormGroup, UntypedFormControl, UntypedFormBuilder, Validators, ValidatorFn } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/shared/services/auth/auth.service';
 
@@ -9,17 +9,17 @@ import { AuthService } from 'src/app/shared/services/auth/auth.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
-  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+  loginForm: UntypedFormGroup;
+  constructor(private fb: UntypedFormBuilder, private authService: AuthService, private router: Router) {
     
   }
   
   ngOnInit(): void {
     this.getCaptcha()
     this.loginForm = this.fb.group({
-      email: new FormControl('',[Validators.required,Validators.maxLength(256),Validators.email]),
-      password: new FormControl('',[Validators.required,Validators.maxLength(256),Validators.minLength(8)]),
-      captcha: new FormControl('',[Validators.required,Validators.maxLength(5),Validators.minLength(5)]),
+      email: new UntypedFormControl('',[Validators.required,Validators.maxLength(256),Validators.email]),
+      password: new UntypedFormControl('',[Validators.required,Validators.maxLength(256),Validators.minLength(8)]),
+      captcha: new UntypedFormControl('',[Validators.required,Validators.maxLength(5),Validators.minLength(5)]),
       //otp: new FormControl('',[Validators.required,Validators.maxLength(8),Validators.minLength(8)])
     })
   }
