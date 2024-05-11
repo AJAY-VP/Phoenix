6fef84d5ae214aa5263cca6728e039aedeae267d
Ajay V P <vpajay.99@gmail.com>
2024-05-11 15:37:31 +0530

    Changes for ec2 deployment

--- backend/userService/app/validator/validatorErrorOrSuccess.js
+++ backend/userService/app/validator/validatorErrorOrSuccess.js
@@ -1,10 +1,10 @@
-const { validationResult } = require('express-validator');
-const ResponseHandler = require('../utility/response.js');
+const {validationResult} = require('express-validator');
+const responseHandler = require('../utility/response.js');
 
-module.exports.Validation = (req,res,next) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        ResponseHandler(res,'failure',errors.array(),422);
-    }
-    next();
-}
+module.exports.Validation = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    responseHandler(res, 'failure', errors.array(), 422);
+  }
+  next();
+};
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  RecaptchaUIKey: "6LcDn0QnAAAAAEh4hR-9sg9ScNT_u5Kh6-Hl4lb8",
+  gatewayUrl: "http://localhost:3000/",
+  secToken: "8a75de1917c62ca068f3fbabdf91b1e4c68f7f02b708ef1bb41fade8660ce826"
 };
